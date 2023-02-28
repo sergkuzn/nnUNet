16d1b0d5d677f0fb0b63d59e3be69ffc38c009bb
Sergei Kuznetsov <segasp5@gmail.com>
2023-02-28 20:30:44 +0100

    prints2 fix3

--- nnunet/training/network_training/nnUNetTrainerV2_my.py
+++ nnunet/training/network_training/nnUNetTrainerV2_my.py
@@ -253,7 +253,7 @@ class nnUNetTrainerV2My(nnUNetTrainerMy):
         data = data_dict['data']
         target = data_dict['target']
 
-        print('Batch', np.unique(target.detach().cpu().numpy()))
+        print('Batch', [np.unique(t) for t in target])
 
         data = maybe_to_torch(data)
         target = maybe_to_torch(target)
