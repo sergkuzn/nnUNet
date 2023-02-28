ae3ec927f120ad59082044aac7585c6e8c770fc6
Sergei Kuznetsov <segasp5@gmail.com>
2023-02-28 19:44:20 +0100

    add weighted ce loss: cmall change

--- nnunet/training/loss_functions/dice_loss.py
+++ nnunet/training/loss_functions/dice_loss.py
@@ -357,8 +357,8 @@ class DC_and_CE_loss(nn.Module):
                 target0 = target[:, 0].long()  # batch x h x w
                 num_classes = net_output.shape[1]  # batch x num_classes x h x w
                 class_count = [(target0 == i).sum().cpu().item() for i in range(num_classes)]
-                weights = np.array([1/count if count != 0 else 0 for count in class_count], dtype=float)
-                weights /= weights.sum()
+                weights = [1/count if count != 0 else 0 for count in class_count]
+                weights = np.array([w / sum(weights) for w in weights], dtype=float)
                 weights = torch.from_numpy(weights).to(target0.device).float()
 
                 ce = RobustCrossEntropyLoss(weight=weights)
